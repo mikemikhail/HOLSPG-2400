39de7477ba73169ca1cc22965c181eef667e425c
mikemikhail <mamikhai@cisco.com>
2021-03-30 01:59:36 -0400

    updated graph colors & legends

--- ML/ML-anomaly_detection-replay/constants.py
+++ ML/ML-anomaly_detection-replay/constants.py
@@ -21,6 +21,8 @@ feature_mean = 0.0
 feature_std = 0.0
 feature_max = 0.0
 
+fig_color = '#d5ffff'   # figures background color = very light blue
+
 tunnel_ifs = ['tunnel-te11200', 'tunnel-te11201', 'tunnel-te13501', 'tunnel-te13502', 'tunnel-te13703', 'tunnel-te13704', 'tunnel-te17801', 'tunnel-te17802', 'tunnel-te12400', 'tunnel-te12401', 'tunnel-te12402', 'tunnel-te12403', 'tunnel-te12404', 'tunnel-te12500', 'tunnel-te12501', 'tunnel-te12502', 'tunnel-te12503', 'tunnel-te12504']
 
 # physical_ifs = ['GigabitEthernet0/0/0/0.1224', 'GigabitEthernet0/0/0/0.1424', 'GigabitEthernet0/0/0/0.1225', 'GigabitEthernet0/0/0/0.1525']
